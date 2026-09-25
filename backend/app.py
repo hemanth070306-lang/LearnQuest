44aaa096328d83e505e7cb4a1643f18fbec77ac9
Sai Hemanth <hemanth@gmail.com>
2026-09-25 07:00:44 +0530

    Fix routes import for Render

--- backend/app.py
+++ backend/app.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
     sys.modules["app"] = sys.modules[__name__]
 
 
-import routes
+from backend import routes
 
 
 # =========================================================
